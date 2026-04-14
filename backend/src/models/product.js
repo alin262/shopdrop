e01b0fb77a02d2edd368a200ec27d76bb5eb56dc
alin26 <alin.joseph.shan25@gmail.com>
2026-04-14 22:29:21 +0530

    feat: add product schema

---
+++ backend/src/models/product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const productSchema= mongoose.Schema({
+name:{
+    type:String,
+    required:true
+},
+description:{
+    type:String,
+    required:true
+},
+price:{
+    type:Number,
+    required:true
+},
+imageUrl:{
+    type:String,
+    default:''
+},
+stock:{
+    type:Number,
+    default:0
+}
+
+},{timestamps:true});
+
+module.exports=mongoose.model('Product',productSchema);
